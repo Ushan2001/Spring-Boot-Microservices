3aab40e79fada35d5e293f17c81738b5b6e80b32
Ushan Mihiranga <ushanmihiranga2017@gmail.com>
2025-02-19 09:07:09 +0530

    create parent project

---
+++ UshanCompany/inventory/src/main/java/com/example/inventory/dto/InventoryDTO.java
@@ -0,0 +1,16 @@
+package com.example.inventory.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class InventoryDTO {
+    private int id;
+    private int itemId;
+    private int productId;
+    private int quantity;
+}
---
+++ UshanCompany/order/src/main/java/com/example/order/model/Orders.java
@@ -0,0 +1,21 @@
+package com.example.order.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+
+public class Orders {
+    @Id
+    private int id;
+    private int itemid;
+    private String orderDate;
+    private int amount;
+}
